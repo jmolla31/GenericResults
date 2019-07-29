d8ce3a3cde0e88ebde152faeb8cb4edc87ce3c02
Javi Mollà <jmolla31@gmail.com>
2019-07-29 10:50:34 +0200

    Version 1.0.2
    Removed unused using statements
    
    Added GenericBatchResult class
    
    Bump version number
    
    Updated .csproj file

---
+++ GenericBatchResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericResults
+{
+    public class GenericBatchResult
+    {
+        public GenericBatchResult(IEnumerable<string> alerts) => this.Alerts = alerts;
+
+        public IEnumerable<string> Alerts { get; } = Enumerable.Empty<string>();
+
+        public bool Succeeded => !this.Alerts.Any();
+    }
+}
